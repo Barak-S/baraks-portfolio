d7f2a4e141c488f27ef576016b825f500d8900c1
Barak Saidoff <barak.saidoff@gmail.com>
2020-03-02 13:19:50 -0500

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Barak Saidoff</h1>
-        <h4>Full Stack Software Engineer</h4>
-        <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Badge_of_the_Israel_Defense_Forces.svg/1200px-Badge_of_the_Israel_Defense_Forces.svg.png"} className="App-logo" alt="logo" />
-        
-      </header>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Projects</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+              </ul>
+              <ul className="navbar-nav">
+              <li className="pull-right linkedIn">
+                <img src="https://www.sharethis.com/wp-content/uploads/2017/05/LinkedIn.png" style={{height: 65, padding:10}} onClick={()=> window.open("https://www.linkedin.com/in/barak-saidoff-3a2a36b2/", "_blank")}></img>
+              </li>
+              <li className="pull-right">
+                <img src="https://nashvillefilmfestival.org/wp-content/uploads/2018/03/Instagram-Button.png" style={{height: 65, padding:10}} onClick={()=> window.open("https://www.instagram.com/barak_saidoff/?hl=en", "_blank")}></img>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      <body>
+          <h1>Barak</h1>
+      </body>
     </div>
   );
 }
