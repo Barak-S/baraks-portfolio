2cb785fc74ea058ff7df28a914ce20b71aa815c9
Barak Saidoff <barak.saidoff@gmail.com>
2020-03-03 11:27:01 -0500

    github link

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="#" style={{fontSize: 18}}>Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Projects</a>
+                <a className="nav-link" href="#" style={{fontSize: 18}}>Projects</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
+                <a className="nav-link" href="#" style={{fontSize: 18}}>About</a>
               </li>
               </ul>
               <ul className="navbar-nav">
@@ -28,12 +28,14 @@ function App() {
               <li className="pull-right">
                 <img src="https://nashvillefilmfestival.org/wp-content/uploads/2018/03/Instagram-Button.png" style={{height: 65, padding:10}} onClick={()=> window.open("https://www.instagram.com/barak_saidoff/?hl=en", "_blank")}></img>
               </li>
+              <li className="pull-right">
+                <img src="https://i.ya-webdesign.com/images/github-icon-png-7.png" style={{height: 65, padding:10}} onClick={()=> window.open("https://github.com/Barak-S", "_blank")}></img>
+              </li>
             </ul>
           </div>
         </nav>
-      <body>
-          <h1>Barak</h1>
-      </body>
+      <h1 style={{paddingTop: 180, fontSize: 65, paddingBottom: 10}}>Barak Saidoff</h1>
+      <p style={{fontSize: 28}}>Full Stack Software Engineer</p>
     </div>
   );
 }
