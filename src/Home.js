5c3f8cf3baea8cc12854d85feec751cf74a2c1b7
Barak Saidoff <barak.saidoff@gmail.com>
2020-05-14 17:33:18 -0400

    carousel img center

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import { Carousel, Card } from 'react-bootstrap';
-// import Contact from './Contact'
-
 
 export default class Home extends React.Component{
 
     state= {
-        projectImages: [{img:"../Soldier.jpeg", description: "Covid-19 Data UI"}, {img: "../homescreen.png", description: "movie app"}, {img: "../addmovie.png", description: "Cozy cabins"}, {img: "../addmovie.png", description: "Pirate Wars"}]
+        projectImages: [{img:"../Covid.png", description: "Covid-19 Data UI"}, {img: "../homescreen.png", description: "movie app"}, {img: "../cozyCabins.png", description: "Cozy cabins"}, {img: "../pirateWarsGame.png", description: "Pirate Wars"}]
     }
 
     mapPictures(){
         return(
             this.state.projectImages.map((img, index) =>{
                 return(
-                    <Carousel.Item>
+                    <Carousel.Item style={{backgroundColor: "#333"}}>
                         <img
-                        className="d-block w-100"
                         src={img.img}
                         alt={ index+1+ "slide"}
-                        style={{ height: 550 }}
+                        style={{display: 'flex', height: 550, display: "block", marginLeft: "auto", marginRight: "auto", width: "50%" }}
                         />
                         <Carousel.Caption>
                         <h3>{img.description}</h3>
@@ -33,50 +30,8 @@ export default class Home extends React.Component{
     render(){
         return(
             <div>
-                {/* <Contact/> */}
                 <Carousel className="carousel" style={{ display: "flex", marginTop:"7%", justifyContent: "center", alignItems: "center", height: 650, paddingRight: "10.5%", paddingLeft: "10.5%" }} >
-                    {/* <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="../Soldier.jpeg"
-                        alt="First slide"
-                        style={{height: 450, width: 200}}
-                        />
-                        <Carousel.Caption>
-                        <h3>Cozy Cabins</h3>
-                        <p>Real Estate Application</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="../homescreen.png"
-                        alt="Third slide"
-                        style={{height: 450, width: 200}}
-                        />
-
-                        <Carousel.Caption>
-                        <h3>Pirate Wars</h3>
-                        <p>JavaScript application game that models after the classic Battleship board game with its own unique touch in styling and gameplay.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src="../addmovie11.png"
-                        alt="Third slide"
-                        style={{height: 450, width: 200}}
-                        />
-
-                        <Carousel.Caption>
-                        <h3>React Native Movie & TV App</h3>
-                        <p>Search Movie and TV show titles and share recommendations with friends</p>
-                        </Carousel.Caption>
-                    </Carousel.Item> */}
-                    
                     {this.mapPictures()}
-
-            
                 </Carousel>
             </div>
         )
