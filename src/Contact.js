0b88a5a17574561ab5176179e0eed3d468053808
Barak Saidoff <barak.saidoff@gmail.com>
2020-05-17 13:36:22 -0400

    image change and contact component

--- src/Contact.js
+++ src/Contact.js
@@ -12,12 +12,14 @@ export default function Contact(){
                         <Container>
                             <Row>
                                 <Col xs={8} md={4}>
-                                <Image src="../images/Soldier.jpeg" thumbnail/>
+                                <Image src="../images/headshot2.jpeg" thumbnail/>
                                 <Card.Title style={{paddingTop:12, color: "#C8C8C8"}}>Barak Saidoff</Card.Title>
                                 <Card.Text style={{color: "#C8C8C8", fontWeight: "600", paddingBottom:8.5}}>Full Stack Software Developer</Card.Text>
                                 </Col>
                                 <Col>
-                                <p style={{color: "#C8C8C8"}}>Experienced in Ruby on Rails, JavaScript and ReactJS/ React Native based programming. Backed by financial services and military leadership.</p>
+                                <p style={{color: "#C8C8C8"}}>I am a full stack web developer experienced in Ruby on Rails, JavaScript and ReactJS/ React Native based programming.</p>
+                                <p style={{color: "#C8C8C8"}}>Background in Sales and Financial Services <strong>|</strong> Partnered with S2 Filings sales team and brought in leads that accounted for generating over $1 million in annual sales.</p>
+                                <p style={{color: "#C8C8C8"}}>Military Leadership <strong>|</strong> Sergeant in the IDF <strong>|</strong> I Achieved award of excellence upon advanced training graduation receiving commander’s Beret.</p>
 
                                 <p style={{color: "#C8C8C8"}}><strong>Technical Skills: </strong>Ruby, Rails, SQL, JavaScript, React, React Native based programming</p>
                                 
@@ -40,3 +42,4 @@ export default function Contact(){
         </div>
     )
 }
+
