c249b3656431f11df57ef748fd415a9e8c1bd3ba
Barak Saidoff <barak.saidoff@gmail.com>
2020-05-20 09:04:33 -0400

    contact rollback

--- src/Contact.js
+++ src/Contact.js
@@ -5,40 +5,49 @@ import './App.css';
 
 export default function Contact(){
     return(
-        <div className="contact">
-        <Card style={{ margin: "3.25%", borderColor: "#e95f5f", backgroundColor: "#343A40"}}>
-            <Card.Header style={{color: "#e95f5f", fontSize: 17, paddingBottom: 8, fontWeight: "700", borderColor: "#000"}}>Contact</Card.Header>
-            <Card.Body>
-                <div>
-                    <Container>
-                        <Row>
-                            <Col xs={8} md={4}>
-                            <Image src="../images/headshot2.jpeg" thumbnail style={{borderColor: "#000"}}/>
-                            <Card.Title style={{paddingTop:12, color: "#e95f5f"}}>Barak Saidoff</Card.Title>
-                            <Card.Text style={{color: "#e95f5f", fontWeight: "600", paddingBottom:8.5}}>Full Stack Software Developer</Card.Text>
-                            </Col>
-                            <Col>
-                            <p style={{color: "#e95f5f"}}>Experienced in Ruby on Rails, JavaScript and ReactJS/ React Native based programming. Background in sales, financial services, and military leadership.</p>
+        <div style={{margin: "3%", paddingBottom: 25}}>
+            <Card className="contactCard">
+                <Card.Body style={{backgroundColor:"#343A40"}}>
+                    <div>
+                        <Container>
+                            <Row>
+                                <Col xs={6} md={3}>
+                                <Image className="profilePicture" src="../images/headshot2.jpeg" thumbnail/>
+                                <Card.Title style={{paddingTop:12, color: "#D3D3D3", fontSize: 25}}>Barak Saidoff</Card.Title>
+                                <Card.Text style={{color: "#D3D3D3", fontWeight: "600", paddingBottom:8.5}}>Full Stack Web Developer</Card.Text>
+                                </Col>
+                                <Col>
+                                <p style={{color: "#D3D3D3"}}>I am a full stack web developer experienced in Ruby on Rails, JavaScript and ReactJS/ React Native based programming.</p>
+                                <p style={{color: "#D3D3D3"}}>Background in Sales and Financial Services <strong>|</strong> Partnered with S2 Filings sales team and brought in leads that accounted for generating over $1 million in annual sales.</p>
+                                <p style={{color: "#D3D3D3"}}>Military Leadership <strong>|</strong> Sergeant in the IDF <strong>|</strong> I Achieved award of excellence upon advanced training graduation receiving commander’s Beret.</p>
 
-                            <p style={{color: "#e95f5f"}}><strong>Technical Skills: </strong>Ruby, Rails, SQL, JavaScript, React, React Native based programming</p>
-                            
-                            <ul className="navbar-nav">
-                                <li>
-                                    <img src="https://www.sharethis.com/wp-content/uploads/2017/05/LinkedIn.png" style={{height: 63, padding:10,}} onClick={()=> window.open("https://www.linkedin.com/in/baraksaidoff/", "_blank")} alt="LinkedIn"></img>
-                                </li>
-                                <li>
-                                    <img src="https://i.ya-webdesign.com/images/github-icon-png-7.png" style={{height: 63, padding:10}} onClick={()=> window.open("https://github.com/Barak-S", "_blank")}alt="GitHub"></img>
-                                </li>
+                                <p style={{color: "#D3D3D3"}}><strong style={{fontSize: 20}}>Technical Skills<br/> </strong>Ruby, Rails, SQL, JavaScript, React, React Native based programming</p>
 
-                            </ul>
-                            </Col> 
-                        </Row>
+                                <p style={{color: "#D3D3D3"}}><strong style={{fontSize: 15}}>Email:<br/> </strong>barak.saidoff@gmail.com</p>
+                                <p style={{color: "#D3D3D3"}}><strong style={{fontSize: 15}}>Resume:<br/> </strong><a href="https://learn.co/baraksaidoff/resume" target="_blank">https://learn.co/baraksaidoff/resume</a></p>
 
-                    </Container>
-                </div>
-            </Card.Body>
-        </Card>
-    </div>
+                                </Col> 
+                            </Row>
+                            <div id="iconList" style={{textAlign: "center"}}>
+                                    <ul id="horizontal-list">
+                                        <li>
+                                            <img src="https://www.sharethis.com/wp-content/uploads/2017/05/LinkedIn.png" className="buttonIcon" style={{height: 63, padding:10,}} onClick={()=> window.open("https://www.linkedin.com/in/baraksaidoff/", "_blank")} alt="LinkedIn"></img>
+                                        </li>
+                                        <li>
+                                            <img src="https://i.ya-webdesign.com/images/github-icon-png-7.png" className="buttonIcon" style={{height: 63, padding:10}} onClick={()=> window.open("https://github.com/Barak-S", "_blank")}alt="GitHub"></img>
+                                        </li>
+                                        <li>
+                                            <img src="https://cdn4.iconfinder.com/data/icons/social-media-2210/24/Medium-512.png" className="buttonIcon" style={{height: 63, padding:10}} onClick={()=> window.open("https://medium.com/@baraksaidoff", "_blank")}alt="Medium"></img>
+                                        </li>
+                                    </ul>
+                                </div>
+                                {/* <Card.Title style={{paddingTop:12, color: "#D3D3D3", textAlign: "center", fontSize: 25.5}}>Projects</Card.Title>
+                            <ProjectCarousel/> */}
+                        </Container>
+                    </div>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 
