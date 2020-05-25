f2a14cd7c280380cb83a3fe032876c8afcd80cd8
Barak Saidoff <barak.saidoff@gmail.com>
2020-05-25 15:46:39 -0400

    row col

--- src/Contact.js
+++ src/Contact.js
@@ -14,19 +14,19 @@ export default function Contact(){
                                 <Col xs={6} md={3}>
                                 <Image className="profilePicture" src="../images/headshot2.jpeg" thumbnail/>
                                 <Card.Title style={{paddingTop:12, color: "#D3D3D3", fontSize: 25}}>Barak Saidoff</Card.Title>
-                                <Card.Text style={{color: "#D3D3D3", fontWeight: "600", paddingBottom:8.5}}>Full Stack Web Developer</Card.Text>
+                                <Card.Text style={{color: "#D3D3D3", fontWeight: "600", paddingBottom:9}}>Full Stack Web Developer</Card.Text>
                                 </Col>
                                 <Col>
-                                <p style={{color: "#D3D3D3"}}>I am a full stack web developer experienced in Ruby on Rails and JavaScript based programming.</p>
-                                <p style={{color: "#D3D3D3"}}>Background in Sales and Financial Services <strong>|</strong> Partnered with S2 Filings sales team and brought in leads that accounted for generating over $1 million in annual sales.</p>
-                                <p style={{color: "#D3D3D3"}}>Military Leadership <strong>|</strong> Sergeant in the IDF <strong>|</strong> I Achieved award of excellence upon advanced training graduation receiving commander’s Beret.</p>
+                                    <p style={{color: "#D3D3D3"}}>I am a full stack web developer experienced in Ruby on Rails and JavaScript based programming.</p>
+                                    <p style={{color: "#D3D3D3"}}>Background in Sales and Financial Services <strong>|</strong> Partnered with S2 Filings sales team and brought in leads that accounted for generating over $1 million in annual sales.</p>
+                                    <p style={{color: "#D3D3D3"}}>Military Leadership <strong>|</strong> Sergeant in the IDF <strong>|</strong> I Achieved award of excellence upon advanced training graduation receiving commander’s Beret.</p>
 
-                                <p style={{color: "#D3D3D3"}}><strong style={{fontSize: 20}}>Technical Skills<br/> </strong>Ruby, Rails, SQLite, PostgreSQL, CSS, HTML5, Bootstrap, JavaScript, React, React Native</p>
+                                    <p style={{color: "#D3D3D3"}}><strong style={{fontSize: 20}}>Technical Skills<br/> </strong>Ruby, Rails, SQLite, PostgreSQL, CSS, HTML5, Bootstrap, JavaScript, React, React Native</p>
 
-                                <p style={{color: "#D3D3D3"}}><strong style={{fontSize: 15}}>Email:<br/> </strong>barak.saidoff@gmail.com</p>
-                                <p style={{color: "#D3D3D3"}}><a href="https://learn.co/baraksaidoff/resume" target="_blank" style={{color: '#0377B5', fontWeight: "600"}}>Download Resume</a></p>
-
-                                </Col> 
+                                    <p style={{color: "#D3D3D3"}}><strong style={{fontSize: 15}}>Email:<br/> </strong>barak.saidoff@gmail.com</p>
+                                    <p style={{color: "#D3D3D3"}}><a href="https://learn.co/baraksaidoff/resume" target="_blank" style={{color: '#0377B5', fontWeight: "600"}}>Download Resume</a></p>
+                                </Col>
+                                
                             </Row>
                             <div id="iconList" style={{textAlign: "center"}}>
                                     <ul id="horizontal-list">
