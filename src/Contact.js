9f8cc258eaa08630413a64ed59be32a8a92d9262
Barak Saidoff <barak.saidoff@gmail.com>
2020-09-20 21:22:54 -0400

    social handles padding top and header

--- src/Contact.js
+++ src/Contact.js
@@ -10,9 +10,9 @@ export default function Contact(){
             <div>
                 <Row style={{margin: 10}}>
                     <Col lg={12} md={12} sm={12} xs={12} style={{ textAlign: "center" }}>
-                        <Image style={{ height:200,width:200 }} src={require("./images/headshot3.png")} roundedCircle alt="Barak Saidoff Profile Picture Headshot"/>
-                        <Card.Title style={{paddingTop:12, color: "#0377B5", fontSize: 25, fontWeight: "600"}}>Barak Saidoff</Card.Title>
-                        <Card.Text style={{color: "#0000000", fontWeight: "600", fontSize: 20, paddingBottom:9}}>Full Stack Web Developer</Card.Text>
+                        <Image style={{ height: 200, width: 200 }} src={require("./images/headshot3.png")} roundedCircle alt="Barak Saidoff Profile Picture Headshot"/>
+                        <Card.Title style={{ paddingTop: 12, color: "#0377B5", fontSize: 25, fontWeight: "600" }}>Barak Saidoff</Card.Title>
+                        <Card.Text style={{color: "#0000000", fontWeight: "600", fontSize: 20, paddingBottom: 20 }}>Full Stack Web Developer</Card.Text>
                     </Col>
                     <Col xs={12} sm={12} md={10} lg={10} className="profileCol">
                         <p style={{color: "#0000000", fontWeight: "600"}}>I am a full stack web developer experienced in Ruby on Rails and JavaScript based programming.</p>
@@ -22,11 +22,10 @@ export default function Contact(){
                         <p style={{color: "#0000000", fontWeight: "600"}}><strong style={{fontSize: 20, color: "#000000"}}>Technical Skills<br/> </strong>Ruby, Rails, SQL, NoSQL, PostgreSQL, Mongo DB, CSS3, HTML5, Bootstrap, JavaScript, React, React Native, Node, Express, and Git workflow based programming.</p>
 
                         <p style={{color: "#0000000", fontWeight: "600"}}><strong style={{fontSize: 15}}>Email:<br/> </strong>barak.saidoff@gmail.com</p>
-                        <p style={{ fontWeight: "600", fontSize: 18}}><a href="https://learn.co/baraksaidoff/resume" target="_blank" title="Barak Saidoff Resume" style={{color: '#0377B5',}}>Download Resume</a></p>
+                        <p style={{ fontWeight: "600", fontSize: 18 }}><a href="https://learn.co/baraksaidoff/resume" target="_blank" title="Barak Saidoff Resume" style={{color: '#0377B5',}}>Download Resume</a></p>
                     </Col>
 
                 </Row>
-                
                 <Projects/>
                 <Blog/>
             </div>
--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div style={{textAlign: "center", fontSize:17, fontWeight: "600", color: "#0377B5"}}>
+        <div style={{ textAlign: "center", fontSize: 17, fontWeight: "600", color: "#0377B5" }}>
             <p>© 2020, Barak Web Development</p>
         </div>
     )
