44d6413bf6231581c547f08343d7c624d0bd5558
Barak-S <barak.saidoff@gmail.com>
2020-10-25 16:07:23 -0400

    rremove small-circle svg

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <div className="App">
         <img class="big-circle" src={Beclipse} alt="" />
         <img class="medium-circle" src={Meclipse} alt="" />
-        <img class="small-circle" src={Seclipse} alt="" />
+        {/* <img class="small-circle" src={Seclipse} alt="" /> */}
           <SocialHandles/>
           <Contact/>
           <Projects/>
