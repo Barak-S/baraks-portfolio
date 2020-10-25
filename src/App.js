d144dd3f787c625828a98f0150c25b4e960b1d9a
Barak-S <barak.saidoff@gmail.com>
2020-10-25 14:51:59 -0400

    added circle svgs

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ import SocialHandles from './SocialHandles';
 import Footer from './Footer';
 import Projects from './Home';
 import Blog from './Blog';
+import Beclipse from './images/big-eclipse.svg'
+import Meclipse from './images/mid-eclipse.svg'
+import Seclipse from './images/small-eclipse.svg'
+
 
 function App() {
   return (
       <div className="App">
+        <img class="big-circle" src={Beclipse} alt="" />
+        <img class="medium-circle" src={Meclipse} alt="" />
+        <img class="small-circle" src={Seclipse} alt="" />
           <SocialHandles/>
           <Contact/>
           <Projects/>
