1353ce9805d56ba7b79a4b7c7945f96579df3b9d
Barak-S <barak.saidoff@gmail.com>
2020-11-02 15:08:36 -0500

    chnaged covid project intro and technologies

--- src/projects.js
+++ src/projects.js
@@ -86,12 +86,12 @@ export const projects = [
         "title": "Covid-19 Data UI", 
         "description": [
             {
-                "intro": "React.js application for tracking and displaying the Covid-19 disease spread and news.",
+                "intro": "This project was created back in march of 2020, when the spread of the Coronavirus was extreemely rapid world wide. This app is intended for tracking daily updated for ever single countries covid-19 infection statistics.",
                 "use" : " The Covid-19 data that is displayed in the app is from an open-source Covid-19 API which can be found at 'https://covid2019-api.herokuapp.com/docs'.",
                 "show" : "By default, the data is displayed in descending order from most confirmed cases to least. Clicking or selecting on the table headers will dynamically re-render the results based on the title of the header. The search bar in the top left will also filter through countires based on the search query from the user."
             }
         ],
-        "technologies": "React.js, open-source Covid-19 statistic API",
+        "technologies": "React.js, React Bootstrap, open-source Covid-19 statistic API",
         "code": "https://github.com/Barak-S/Covid-19-UI",
         "live": "https://covid-19-ui.now.sh/",
         "details": [ 
