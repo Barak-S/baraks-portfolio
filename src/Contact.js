8d10be10aa14fb5d2bccbb4c5480b148aee25fc5
Barak-S <barak.saidoff@gmail.com>
2020-11-12 10:08:22 -0500

    intro bg-image with greyscale gradient

--- src/Contact.js
+++ src/Contact.js
@@ -10,8 +10,8 @@ export default function Contact(){
     let introText2 = useRef(null)
 
     useEffect(()=>{
-        TweenMax.staggerTo([introText1,introText2], 1, { opacity: 1, y: -75, ease: Power1.easeIn }, .8)
-        TweenMax.to( intro, 1, { y: '-110vh', ease: Power3.easeIn, delay: 2,})
+        TweenMax.staggerTo([introText1,introText2], 1, { opacity: 1, y: -75, ease: Power1.easeIn }, 1)
+        TweenMax.to( intro, 1, { y: '-110vh', ease: Power3.easeIn, delay: 2.3,})
     }, [])
 
     return(
@@ -43,4 +43,3 @@ export default function Contact(){
         </div>   
     )
 }
-
