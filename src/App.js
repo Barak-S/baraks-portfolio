b1f51f491970227cc5c4aae256ad7bb041be7751
Barak-S <barak.saidoff@gmail.com>
2021-02-16 15:19:15 -0500

    useEffect to ensure autoplay

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Contact from './Contact';
@@ -7,19 +7,28 @@ import Footer from './Footer';
 import Blog from './Blog';
 import Cards from './Cards'
 import Video from './images/video.mp4'
-
-
+import { Player } from 'video-react';
+ 
 function App() {
 
   const videoRef= useRef();
 
-  const setPlayBack = () => {
+  useEffect(()=>{
+
+    const player = videoRef.current.children[0]
+
+    videoRef.current.play()
     videoRef.current.playbackRate = 0.75;
-  };
+    player.setAttribute("muted", "")
+    player.autoplay = true;
+    player.controls = false;
+    player.playsinline = true;
+    player.muted = true;
+  })
 
   return (
       <div className="App">
-        <video ref={videoRef} id="background-video" autoPlay={true} loop muted={true} >
+        <video ref={videoRef} id="background-video" autoPlay muted playsInline loop >
           <source src={Video} type="video/mp4" />
         </video>
         <SocialHandles/>
