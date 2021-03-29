2ec21d96af4d6c88f357e2fc05c5662d6521150d
Barak-S <barak.saidoff@gmail.com>
2021-03-29 19:05:41 -0400

    bio refactor mobile

--- src/Contact.js
+++ src/Contact.js
@@ -13,13 +13,13 @@ export default function Contact(){
             </Col>
             <Col xs={12} sm={12} md={10} lg={9} className="about-banner">
                 <section className="about-section">
-                    <p>A creative Full Stack Developer with an entrepreneurial spirit, proficient at Web Application Development using Git and modern web tools. With attention to detail, I take ideas, deliver quickly and consistently, and build things that provide a lot of value in a short amount of time.</p>
-                    <p><strong style={{fontSize: 17, fontWeight: "700"}}>Background in Sales and Financial Services |</strong> Partnered with S2 Filings sales team and brought in leads that accounted for generating over $1 million in annual sales.</p>
-                    <p><strong style={{fontSize: 17, fontWeight: "700"}}>Military Leadership | Former Sergeant in the IDF |</strong> Achieved award of excellence upon advanced training graduation. </p>
+                    <p className="headline">A creative Full Stack Developer with an entrepreneurial spirit, proficient at Web Application Development using Git and modern web tools. With attention to detail, I take ideas, deliver quickly and consistently, and build things that provide a lot of value in a short amount of time.</p>
+                    <p><strong style={{fontSize: 17, fontWeight: "700"}}>Background in Sales and Financial Services <span style={{ color: "#0377B5"}}>|</span></strong> Partnered with S2 Filings sales team and brought in leads that accounted for generating over $1 million in annual sales.</p>
+                    <p><strong style={{fontSize: 17, fontWeight: "700"}}>Military Leadership <span style={{ color: "#0377B5"}}>|</span> Former Sergeant in the IDF <span style={{ color: "#0377B5"}}>|</span></strong> Achieved award of excellence upon advanced training graduation. </p>
                     <div style={{textAlign: "center"}}>
                         <p className="skills-banner">Technical Skills</p>
                         <p>JavaScript, React, React Native, Redux, TypeScript, Node, Express, Ruby, Rails, SQL, NoSQL, CSS3, SASS, HTML5, GSAP, Bootstrap, Responsive Design, and Git workflow based programming.</p>
-                        <button className="live-button" onClick={()=> window.open("https://learn.co/baraksaidoff/resume", "_blank")} style={{ marginTop: 36}}>Download Resume</button>
+                        <button className="live-button" onClick={()=> window.open("https://learn.co/baraksaidoff/resume", "_blank")} style={{ marginTop: 20}}>Download Resume</button>
                     </div>
                 </section>
             </Col>  
